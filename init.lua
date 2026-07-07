6b2f20c5ea9b2e4de07d5e9faaa5588499ce3121
Andrew Muncie <muncie88@hotmail.com>
2026-07-07 16:37:36 +0100

    initial modifications from t14s

--- init.lua
+++ init.lua
@@ -343,6 +343,12 @@ do
   --
   -- We first install it from https://github.com/NMAC427/guess-indent.nvim
   -- and then call its `setup()` function to start it with default settings.
+  vim.pack.add { gh 'vimwiki/vimwiki' }
+    vim.g.vimwiki_path = '$HOME/.local/share/vimwiki'
+    vim.g.vimwiki_syntax = 'markdown'
+    vim.g.vimwiki_ext = 'md'
+  --    
+  -- 
   vim.pack.add { gh 'NMAC427/guess-indent.nvim' }
   require('guess-indent').setup {}
 
@@ -967,11 +973,12 @@ do
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
   -- require 'kickstart.plugins.debug'
-  -- require 'kickstart.plugins.indent_line'
+  require 'kickstart.plugins.indent_line'
   -- require 'kickstart.plugins.lint'
-  -- require 'kickstart.plugins.autopairs'
-  -- require 'kickstart.plugins.neo-tree'
-  -- require 'kickstart.plugins.gitsigns' -- adds gitsigns recommended keymaps
+  require 'kickstart.plugins.autopairs'
+  require 'kickstart.plugins.neo-tree'
+  require 'kickstart.plugins.gitsigns' -- adds gitsigns recommended keymaps
+  
 
   -- NOTE: You can add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --
